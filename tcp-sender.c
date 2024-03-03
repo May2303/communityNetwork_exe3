6aab96fa52d23f139c89387ceb511ed796b587f6
DanielKuris <danielkuris6@gmail.com>
2024-03-03 20:01:19 +0200

    Added prints and fixed while loop

--- tcp-sender.c
+++ tcp-sender.c
@@ -38,17 +38,14 @@ void generate_random_file(const char *filename, size_t size) {
 void print_statistics(clock_t start_time, clock_t end_time, size_t file_size_bytes) {
     double elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
     double bandwidth = file_size_bytes / elapsed_time;
-    printf("Time taken: %.2f seconds\n", elapsed_time);
-    printf("Average bandwidth: %.2f bytes/second\n", bandwidth);
+    printf("Time taken: %.2f ms\n", elapsed_time);
 }
 
 int main(int argc, char *argv[]) {
-    printf("First line");
     if (argc != 7 || strcmp(argv[1], "-ip") != 0 || strcmp(argv[3], "-port") != 0 || strcmp(argv[5], "-algo") != 0) {
-        printf("Usage: %s -ip <receiver_ip> -port <receiver_port> -algo <congestion_algorithm>\n", argv[0]);
+        printf("Usage: %s -ip <ip> -port <receiver_port> -algo <congestion_algorithm>\n", argv[0]);
         return -1;
     }
-    printf("Reading input..");
 
     const char *receiver_ip = argv[2];
     int receiver_port = atoi(argv[4]);
@@ -58,70 +55,97 @@ int main(int argc, char *argv[]) {
     // Generate random file of at least 2MB size
     size_t file_size_bytes = BUFFER_SIZE;
     generate_random_file(file_name, file_size_bytes);
-    printf("Generated random file");
 
-    // Create a TCP socket
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        perror("Error creating socket");
-        return -1;
-    }
+    
 
-    if (strcmp(congestion_algorithm, "reno") == 0) {
-        setsockopt(sock, IPPROTO_TCP, congestion_algorithm, "reno", strlen("reno"));
-    } else if (strcmp(congestion_algorithm, "cubic") == 0) {
-        setsockopt(sock, IPPROTO_TCP, congestion_algorithm, "cubic", strlen("cubic"));
-    } else {
-        printf("Invalid congestion control algorithm specified.\n");
-        close(sock);
-        return -1;
-    }
+    char decision='n';
+    while (1) {
+        // Create a TCP socket
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == -1) {
+            perror("Error creating socket");
+            return -1;
+        }
 
-    // Set up connection parameters
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(receiver_port);
-    inet_pton(AF_INET, receiver_ip, &server_address.sin_addr);
-
-    // Connect to the receiver
-    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
-        perror("Error connecting to receiver");
-        close(sock);
-        return -1;
-    }
-
-    // Send the file
-    clock_t start_time = clock();
-    FILE *file = fopen(file_name, "rb");
-    if (file == NULL) {
-        perror("Error opening file for reading");
-        close(sock);
-        return -1;
-    }
-
-    char buffer[BUFFER_SIZE];
-    size_t bytes_read;
-    while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-        ssize_t bytes_sent = send(sock, buffer, bytes_read, 0);
-        if (bytes_sent == -1) {
-            perror("Error sending file");
-            fclose(file);
+        if (strcmp(congestion_algorithm, "reno") == 0) {
+            setsockopt(sock, IPPROTO_TCP, congestion_algorithm, "reno", strlen("reno"));
+        } else if (strcmp(congestion_algorithm, "cubic") == 0) {
+            setsockopt(sock, IPPROTO_TCP, congestion_algorithm, "cubic", strlen("cubic"));
+        } else {
+            printf("Invalid congestion control algorithm specified.\n");
             close(sock);
             return -1;
         }
+
+        // Set up connection parameters
+        struct sockaddr_in server_address;
+        memset(&server_address, 0, sizeof(server_address));
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(receiver_port);
+        inet_pton(AF_INET, receiver_ip, &server_address.sin_addr);
+
+        // Connect to the receiver
+        if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+            perror("Error connecting to receiver");
+            close(sock);
+            return -1;
+        }    
+        
+        if(decision == 'y')
+            printf("Continuing . . .\n");
+
+
+        printf("Sending %ld bytes of data . . .\n", BUFFER_SIZE);
+        // Send the file
+        clock_t start_time = clock();
+        FILE *file = fopen(file_name, "rb");
+        if (file == NULL) {
+            perror("Error opening file for reading");
+            close(sock);
+            return -1;
+        }
+
+        char buffer[BUFFER_SIZE];
+        size_t bytes_read;
+        while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+            ssize_t bytes_sent = send(sock, buffer, bytes_read, 0);
+            if (bytes_sent == -1) {
+                perror("Error sending file");
+                fclose(file);
+                close(sock);
+                return -1;
+            }
+        }
+
+        fclose(file);
+        clock_t end_time = clock();
+
+        printf("Successfully sent %ld bytes of data!\n", file_size_bytes);
+
+        // Calculate and print statistics
+        print_statistics(start_time, end_time, file_size_bytes);
+
+        // Ask user if they want to send more data
+        printf("Do you want to send more data? (y/n): ");
+        scanf(" %c", &decision);
+
+        if (decision != 'y' && decision != 'n') {
+            printf("Invalid answer, do you want to send more data? (y/n) ");
+            scanf(" %c", &decision);
+        }
+
+        if (decision == 'n')
+            break;
+        if(decision == 'y')
+            printf("Waiting for the server to be ready . . .\n");
+
+        // Close the TCP connection
+        close(sock);
     }
 
-    fclose(file);
-    clock_t end_time = clock();
 
-    printf("Successfully sent %ld bytes of data!\n", file_size_bytes);
-
-    // Calculate and print statistics
-    print_statistics(start_time, end_time, file_size_bytes);
-
-    // Close the TCP connection
-    close(sock);
+    printf("Exited program.\n");
+    
 
     return 0;
 }
